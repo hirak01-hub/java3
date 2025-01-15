56092ff8f2681df852e0db68ce5b7da42c3bb33f
hirak01-hub <hirakmodak123@gmail.com>
2025-01-15 16:12:29 +0530

    CenteredPyramid.java

---
+++ CenteredPyramid.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CenteredPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        
+        // Centered pyramid
+        for (int i = 1; i <= rows; i++) {
+            // Print leading spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
